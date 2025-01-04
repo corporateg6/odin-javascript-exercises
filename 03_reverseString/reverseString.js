8d54a4b4f84b949d1105314dec5f879499c245b6
corporateg6 <george.brently@gmail.com>
2025-01-04 02:59:08 -0500

    complete ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(aStr) {
+    let strArr = aStr.split("");
+    let revArr = new Array();
 
+    while (strArr.length > 0) {
+        revArr.push(strArr.pop());
+    }
+
+    return revArr.join("");
 };
 
 // Do not edit below this line
