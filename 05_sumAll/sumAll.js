b3271907f6f2681a94ba009da8af873fbe651964
corporateg6 <george.brently@gmail.com>
2025-01-04 15:47:17 -0500

    complete ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let ret = 0;
+    if (!Number.isInteger(start) ||
+        !Number.isInteger(end) ||
+        start < 0 ||
+        end < 0) {
+            ret = "ERROR";
+        } else {
+            if (end < start) {
+                let temp = start;
+                start = end;
+                end = temp;
+            }
+            for (let i = start; i < end+1; i++) {
+                ret += i;
+            }
+        }
+    return ret;
 };
 
 // Do not edit below this line
