a1cb40488e73564ae87bd61ad925c906b3944e1f
corporateg6 <george.brently@gmail.com>
2025-01-04 15:34:53 -0500

    complete ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(inArr, ...inArg) {
+    let ret = new Array();
+    for (let i = 0; i < inArr.length; i++) {
+        if (!(inArg.includes(inArr[i]))) {
+            ret.push(inArr[i]);
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
