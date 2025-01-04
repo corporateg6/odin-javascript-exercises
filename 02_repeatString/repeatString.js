6df8549c957fe2398d3d64e02e9c233f66956089
corporateg6 <george.brently@gmail.com>
2025-01-04 02:49:06 -0500

    complete ex1 and ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(aStr, count) {
+    let ret = '';
+    if (count < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < count; i++) {
+        ret += aStr;
+    }
+    return ret;
 };
 
 // Do not edit below this line
