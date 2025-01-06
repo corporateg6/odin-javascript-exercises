a4f13c4db16fbd47b4a63f672c6a9f85c0010c29
corporateg6 <george.brently@gmail.com>
2025-01-06 06:12:36 -0500

    complete ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const now = new Date(Date.now());
+    const thisYear = now.getFullYear();
 
+    function getElder(elder, person) {     
+        function getAge(person) {
+            if (!person.yearOfDeath) {
+                return thisYear - person.yearOfBirth;
+            } else {
+                return person.yearOfDeath - person.yearOfBirth;
+            }
+        }
+
+        if (getAge(elder) >= getAge(person)) {
+            return elder
+        } else {
+            return person
+        }
+    }
+
+    return array.reduce(getElder);
 };
 
 // Do not edit below this line
