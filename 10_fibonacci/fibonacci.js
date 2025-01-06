904be681bdfd808dcf840bbaf5ad69c20be23727
corporateg6 <george.brently@gmail.com>
2025-01-06 04:57:10 -0500

    complete ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    function fibRec(n) {
+        if (n==0) {
+            return 0;
+        } else if (n==1) {
+            return 1;
+        } else {
+            return fibRec(n-1) + fibRec(n-2);
+        }
+    }
+
+    if (nth<0) {
+        return "OOPS";
+    } else {
+        return fibRec(nth);
+    }
 
 };
 
