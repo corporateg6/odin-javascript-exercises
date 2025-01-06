1f9cff09579fb286f915a49199c7d67d82d06c1d
corporateg6 <george.brently@gmail.com>
2025-01-05 19:10:27 -0500

    complete ex08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sub = 0;
+  arr.forEach((num)=>{
+    sub += num;
+  });
+  return sub;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sub = 1;
+  arr.forEach((num)=>{
+    sub *= num;
+  });
+  return sub;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sub = 1;
+	if (num === 0) {
+    return sub;
+  } else {
+    for (let i = num; i > 0; i--) {
+      sub *= i;
+    }
+    return sub;
+  }
 };
 
 // Do not edit below this line
